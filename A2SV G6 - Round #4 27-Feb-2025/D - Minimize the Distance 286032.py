7bd234829715b7eb8ee4c3150dd39f46ba92460d
Eden344 <130092241+Eden344@users.noreply.github.com>
2025-02-27 16:53:22 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 286032.py
@@ -0,0 +1,31 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+ 
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())) for i in range(n))
+
+case = integer()
+arr = List()
+
+arr.sort()
+
+ans = arr[(case - 1)//2]
+
+
+
+
+
+print(ans)
+
+    
+    
+    
+    
