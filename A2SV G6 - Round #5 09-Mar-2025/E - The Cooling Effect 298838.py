f1a1f6b6371b90c48389a92b89df73881364b299
Eden344 <130092241+Eden344@users.noreply.github.com>
2025-03-09 21:16:38 +0300

    Created 'E - The Cooling Effect' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/E - The Cooling Effect 298838.py
@@ -0,0 +1,45 @@
+# Problem: E - The Cooling Effect - https://codeforces.com/gym/591928/problem/E
+
+import sys
+
+def solve():
+    input = sys.stdin.read
+    data = input().split("\n")
+    index = 0
+    q = int(data[index])
+    index += 1
+    results = []
+    
+    for _ in range(q):
+        if data[index] == "":
+            index += 1  
+        
+        n, k = map(int, data[index].split())
+        index += 1
+        a = list(map(int, data[index].split()))
+        index += 1
+        t = list(map(int, data[index].split()))
+        index += 1
+        
+        
+        inf = float('inf')
+        temperatures = [inf] * n
+        
+        
+        for i in range(k):
+            temperatures[a[i] - 1] = t[i]
+        
+        
+        for i in range(1, n):
+            temperatures[i] = min(temperatures[i], temperatures[i-1] + 1)
+        
+        
+        for i in range(n-2, -1, -1):
+            temperatures[i] = min(temperatures[i], temperatures[i+1] + 1)
+        
+        results.append(" ".join(map(str, temperatures)))
+    
+    sys.stdout.write("\n".join(results) + "\n")
+
+if __name__ == "__main__":
+    solve()
